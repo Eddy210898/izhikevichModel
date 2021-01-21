2e831b6b8224b40f4c37651e1e158657d47cca3f
Eddy Macario <eddy.macario@alumnos.uneatlantico.es>
2021-01-22 00:58:20 +0100

    public properties

--- src/izhikevichParams.h
+++ src/izhikevichParams.h
@@ -3,9 +3,9 @@
 
 class izhikevichParams
 {
-    float a = 0.02, b = 0.2, c = -65, d = 2;
 
 public:
+    float a = 0.02, b = 0.2, c = -65, d = 2;
     float getA();
     float getB();
     float getC();
