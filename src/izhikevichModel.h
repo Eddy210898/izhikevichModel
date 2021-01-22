024c7b111f2985077b8dd6ea513edd8c6a9d0fc4
Eddy Macario <eddy.macario@alumnos.uneatlantico.es>
2021-01-22 01:41:12 +0100

    Add missing library

--- src/izhikevichModel.h
+++ src/izhikevichModel.h
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 #include "izhikevichStep.h"
 using namespace std;
 vector<float> izhikevichModel(float vO, float uO, float dT, izhikevichParams params, int start = 0, int stop = 2000, float I = 0);
